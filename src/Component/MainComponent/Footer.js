d81ef266325bdd40997ad51f51ce59d4264774fd
= <=>
2024-10-02 11:23:42 +0530

    :::Header Footer Home Master Page Create:::

---
+++ src/Component/MainComponent/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      Hello Footer Page
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/Component/MainComponent/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      Hello Header Page
+    </div>
+  )
+}
+
+export default Header
